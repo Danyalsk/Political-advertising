66c862840258f7b209acc786ef2eb06fc089e564
danyal sameer khalife <141676015+Danyalsk@users.noreply.github.com>
2024-04-03 19:16:25 +0530

    Update padding and font size in App.css, add description1 in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,19 @@ import MapChart from "./map";
 
 function App() {
   return (
-    <div className="landing-page ">
+    <div className="landing-page">
       <div className="content">
-        <h1 className="title">India PA</h1>
+        <h1 className="title">India Political Advertisement Map</h1>
         <p className="description">
-          Explore data on political advertisements in India.
+          Welcome to the India Political Advertisement Map. This map displays
+          data on political advertisements across India. Explore different
+          regions and discover insights into political advertising trends.
         </p>
+        <div className="description1">
+          <h2>Click on the pins to view details of political advertisers.</h2>
+        </div>
         <div style={{ position: "relative" }}>
+          {/* Pass handlePinClick function as a prop to MapChart */}
           <MapChart style={{ position: "relative" }} />
         </div>
       </div>
